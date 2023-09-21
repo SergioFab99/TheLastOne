4b8b59b186c85d4ef71d41c9a01e61042cde90a3
Sergio Fabián <checho.500@hotmail.com>
2023-09-21 01:33:48 -0500

    logica de bala y sprites

--- Assets/Scripts/Jugador.cs
+++ Assets/Scripts/Jugador.cs
@@ -1,26 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using System.Collections;
 
 public class Jugador : MonoBehaviour
 {
     public float velocidadMovimiento = 5.0f;
+    private SpriteRenderer spriteRenderer; 
+    private bool mirandoDerecha = false; 
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>(); 
+    }
 
     void Update()
     {
-        // Obtén las entradas de teclado o controles
         float movimientoHorizontal = Input.GetAxis("Horizontal");
         float movimientoVertical = Input.GetAxis("Vertical");
 
-        // Calcula la dirección del movimiento
         Vector2 direccion = new Vector2(movimientoHorizontal, movimientoVertical).normalized;
-
-        // Calcula la velocidad de movimiento
         Vector2 velocidad = direccion * velocidadMovimiento;
 
-        // Aplica la velocidad al Rigidbody2D del jugador
         Rigidbody2D rb2d = GetComponent<Rigidbody2D>();
         rb2d.velocity = velocidad;
+
+        if (movimientoHorizontal > 0 && !mirandoDerecha)
+        {
+            spriteRenderer.flipX = true;
+            mirandoDerecha = true;
+        }
+        else if (movimientoHorizontal < 0 && mirandoDerecha)
+        {
+            spriteRenderer.flipX = false;
+            mirandoDerecha = false;
+        }
     }
 }
+
+
+
