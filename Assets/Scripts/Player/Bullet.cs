a12655bbcc3c27563347deb898a0eb88f0e472f2
Sergio Fabián <checho.500@hotmail.com>
2023-10-02 12:32:43 -0500

    pistola movimiento

---
+++ Assets/Scripts/Player/Bullet.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private new Rigidbody2D rigidbody;
+    public float speed = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rigidbody.MovePosition(transform.position + transform.right * speed * Time.fixedDeltaTime);
+    }
+}
