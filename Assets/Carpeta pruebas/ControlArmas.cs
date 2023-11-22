e469e8219cdbdfb97fcc7b85231f2fd989902009
Sergio Fabián <checho.500@hotmail.com>
2023-11-21 19:57:42 -0500

    pickup de arma

--- Assets/Carpeta pruebas/ControlArmas.cs
+++ Assets/Carpeta pruebas/ControlArmas.cs
@@ -6,7 +6,7 @@ public class ControlArmas : MonoBehaviour
 {
     public GameObject pistola, metralleta, bazooka;
     private int armaSeleccionada = 0;
-
+    [SerializeField] private int NivelDeArma = 1;
     void Start()
     {
         // Al inicio, activa solo la pistola
@@ -15,21 +15,50 @@ public class ControlArmas : MonoBehaviour
 
     void Update()
     {
-        float scroll = Input.GetAxis("Mouse ScrollWheel");
-        if (scroll != 0f)
+        if(Input.GetKeyDown(KeyCode.L))
         {
-            if (scroll > 0f)
-            {
-                armaSeleccionada++;
-                if (armaSeleccionada > 2) armaSeleccionada = 0;
-            }
-            else
-            {
-                armaSeleccionada--;
-                if (armaSeleccionada < 0) armaSeleccionada = 2;
-            }
-            CambiarArma(armaSeleccionada);
+            armaSeleccionada++;
         }
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        if (scroll != 0f && NivelDeArma != 1)
+        {
+            switch(NivelDeArma)
+            {
+                case 2:
+                    if (scroll > 0f)
+                    {
+                        armaSeleccionada++;
+                        if (armaSeleccionada > 1) armaSeleccionada = 0;
+                    }
+                    else
+                    {
+                        armaSeleccionada--;
+                        if (armaSeleccionada < 0) armaSeleccionada = 1;
+                    }
+                    CambiarArma(armaSeleccionada);
+                    break;
+                case 3:
+                    if (scroll > 0f)
+                    {
+                        armaSeleccionada++;
+                        if (armaSeleccionada > 2) armaSeleccionada = 0;
+                    }
+                    else
+                    {
+                        armaSeleccionada--;
+                        if (armaSeleccionada < 0) armaSeleccionada = 2;
+                    }
+                    CambiarArma(armaSeleccionada);
+                    break;
+                default:
+                    Debug.Log("Error");
+                    break;
+            }
+        }
+    }
+    public void UpNivelDeArma()
+    {
+        NivelDeArma++;
     }
 
     void CambiarArma(int arma)
